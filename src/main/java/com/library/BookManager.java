e2f74d2faffc9f761c37a0cfc19c459727d31b03
Emanuele Pippa <81412496+epippa@users.noreply.github.com>
2025-07-05 13:15:24 +0200

    Update BookManager.java

--- src/main/java/com/library/BookManager.java
+++ src/main/java/com/library/BookManager.java
@@ -19,20 +19,22 @@ public class BookManager {     //manages the SQL operations about the Book table
     }
 
     //searches book by title (case-insensitive -> ILIKE)
-    public void searchBooks(String title) {
-        String query="SELECT isbn,title,yearofpublication,copies FROM book WHERE title ILIKE ?";
-        try (PreparedStatement stmt=dbConn.prepareStatement(query)) {
-            stmt.setString(1,"%" + title + "%");    // '%' is used for partial match: any characters before or after the title
+     public void searchBooks(String title) {
+        String query = "SELECT isbn, title, yearofpublication, copies, availableCopies, edition FROM book WHERE title ILIKE ?";
+        try (PreparedStatement stmt = dbConn.prepareStatement(query)) {
+            stmt.setString(1, "%" + title + "%");    //'%' is used for partial match: any characters before or after the title
             
-            try(ResultSet rs=stmt.executeQuery()) {
-                List<Book> results=new ArrayList<>();
+            try (ResultSet rs = stmt.executeQuery()) {
+                List<Book> results = new ArrayList<>();
                 //For each result, create a Book object and add it to the list
-                while(rs.next()) {
+                while (rs.next()) {
                     results.add(new Book(
                         rs.getString("isbn"),
                         rs.getString("title"),
                         rs.getInt("yearofpublication"),
-                        rs.getInt("copies")
+                        rs.getInt("copies"),
+                        rs.getInt("availableCopies"),
+                        rs.getInt("edition")
                     ));
                 }
                 
@@ -48,14 +50,15 @@ public class BookManager {     //manages the SQL operations about the Book table
     }
 
     //Add new book to database
-    public void addNewBook(String isbn,String title,int year,int copies) {
-        String query="INSERT INTO Book (isbn,title,yearofpublication,copies, availableCopies) VALUES (?,?,?,?,?)";
+    public void addNewBook(String isbn,String title,int year,int copies, int edition) {
+        String query="INSERT INTO Book (isbn,title,yearofpublication,copies,availableCopies,edition) VALUES (?,?,?,?,?,?)";
         try(PreparedStatement stmt=dbConn.prepareStatement(query)) {
             stmt.setString(1,isbn);
             stmt.setString(2,title); 
             stmt.setInt(3,year);
             stmt.setInt(4,copies);
             stmt.setInt(5,copies);  //availableCopies initally setted = copies
+            stmt.setInt(6, edition);
             
             int rows=stmt.executeUpdate();  //Executes the prepared INSERT statement and returns how many rows were affected
             System.out.println(rows+" book added!");    //If rows>0, the book was added successfully
@@ -64,20 +67,20 @@ public class BookManager {     //manages the SQL operations about the Book table
         }
     }
 
-    //update the total number of copies of a book owned by the library
-    public void updateCopies(String isbn,int copies) {
-        String query="UPDATE book SET copies=? WHERE isbn=?";
-        try(PreparedStatement stmt=dbConn.prepareStatement(query)) {
-            stmt.setInt(1,copies);
-            stmt.setString(2,isbn);
-            
-            int rows=stmt.executeUpdate();
-            if(rows>0) {
-                System.out.println("Updated!");
+    //update the number of available copies of a book
+    public void updateAvailableCopies(String isbn, int available) {
+        String query = "UPDATE book SET availableCopies = ? WHERE isbn = ?";
+        try (PreparedStatement stmt = dbConn.prepareStatement(query)) {
+            stmt.setInt(1, available);
+            stmt.setString(2, isbn);
+
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Available copies updated!");
             } else {
                 System.out.println("ISBN not found");
             }
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             System.err.println("Update failed");
         }
     }
